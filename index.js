a5ca1851c190b2142db4a02abcaefebb4ca0166a
Darya Rosikhina <drosikhina@meetup.com>
2019-01-26 20:00:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"]
 appenddrivers.push["Ralph"]
-appenddriversdrivers.unshift["Bob"]
-drivers.pop[]
-drivers.shift[]
+appenddrivers.unshift["Bob"]
+appenddrivers.pop
+appenddrivers.shift
